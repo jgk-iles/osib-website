d93384fb6b653c1b68a1a5ce50ecd0811df00803
Joseph Iles <jgk.iles@gmail.com>
2021-07-05 20:13:30 +0100

    Added pagination on blog list view and updated theme to material kit

--- osib/blog/views.py
+++ osib/blog/views.py
@@ -1,10 +1,16 @@
+from django.core import paginator
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Post
 
 
 def post_list(request):
     posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    paginator = Paginator(posts, 5)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'blog/post/list.html', {'page_obj': page_obj})
 
 
 def post_detail(request, year, month, day, post):
--- osib/osib/settings.py
+++ osib/osib/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'osib.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'osib/templates', ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,6 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "osib/static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
