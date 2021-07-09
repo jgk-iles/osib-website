5611c5f6c8fcaadf1e1a8989b8598bb377b3ccae
Joseph Iles <jgk.iles@gmail.com>
2021-07-10 00:05:01 +0100

    Increased image caption length

--- osib/blog/models.py
+++ osib/blog/models.py
@@ -31,7 +31,7 @@ class Post(models.Model):
                               choices=STATUS_CHOICES,
                               default='draft')
     image = models.ImageField(upload_to='posts/%Y/%m/%d/', blank=True)
-    image_caption = models.CharField(max_length=250, blank=True)
+    image_caption = models.CharField(max_length=300, blank=True)
 
     objects = models.Manager()
     published = PublishedManager()
