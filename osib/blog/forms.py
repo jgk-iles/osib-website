5ecc26b325ff366f99d211fcbe221e795f19e838
Joseph Iles <jgk.iles@gmail.com>
2021-07-10 10:00:18 +0100

    Added placeholder text for image caption input

--- osib/blog/forms.py
+++ osib/blog/forms.py
@@ -1,10 +1,13 @@
-from django import forms
+from django.forms import ModelForm, TextInput
 from django.db.models import fields
 from .models import Post
 
 
-class CreatePostForm(forms.ModelForm):
+class CreatePostForm(ModelForm):
 
     class Meta:
         model = Post    
         fields = ('title', 'body', 'status', 'image', 'image_caption',)
+        widgets = {
+            'image_caption': TextInput(attrs={'placeholder': '[Optional] Add image caption'})
+        }
